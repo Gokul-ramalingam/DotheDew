ed6cf414dbd3c1f89d24d7936469d5d467f27a9d
Gokul <gokulramalingam1@gmail.com>
2019-09-18 20:10:48 +0530

    addition of key press function

--- canvas.js
+++ canvas.js
@@ -7,17 +7,25 @@ class Options{
     this.rockImage =  new Image();
     this.paperImage  = new Image();
     this.scissorImage = new Image();
+    this.selectImage = new Image();
     this.rockImage.src = 'images/rock.png';
-    this.paperImage.src = 'images/paper.png'
-    this.scissorImage.src = 'images/scissor.png'
-    // this.selectImage.src = 'images/select.png'
+    this.paperImage.src = 'images/paper.png';
+    this.scissorImage.src = 'images/scissor.png';
+    this.selectImage.src = 'images/select.png';
     onload=()=>{
         this.ctx.drawImage(this.rockImage,175,25,100,100);
         this.ctx.drawImage(this.paperImage,425,25,100,100);
         this.ctx.drawImage(this.scissorImage,670,25,100,100);
-        this.ctx.drawImage(this.scissorImage,425,400,100,100);
+        this.ctx.drawImage(this.selectImage,425,390,100,100);
+    } 
+    this.keyPress = function(e){
+       console.log(e);
     }
-    
 }
+
 }
 let option = new Options();
+
+addEventListener("keydown",function(event){
+    option.keyPress(event.keyCode);     
+   })
