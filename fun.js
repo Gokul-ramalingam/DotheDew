79d29243a969205007db9a4d26f9ce9b083390d4
Pratikshatippe <54614091+Pratikshatippe@users.noreply.github.com>
2019-09-19 15:54:44 +0530

    using class and object

--- fun.js
+++ fun.js
@@ -1,47 +1,42 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-// rock is displayed
-rock_image = new Image();
-rock_image.src = 'images/rock.png';
-rock_image.onload = function(){
-ctx.drawImage(rock_image, 20, 20, 170, 130);
-}
-// this image for paper
-paper_image = new Image();
-paper_image.src = 'images/paper.png';
-paper_image.onload = function(){
-ctx.drawImage(paper_image, 210, 20, 170, 130);
-}
-// this image for scissor
-scissor_image = new Image();
-scissor_image.src = 'images/scissor.png';
-scissor_image.onload = function(){
-ctx.drawImage(scissor_image, 400, 20, 170, 130);
-}
-// this for point to above images
-point_image = new Image();
-
-point_image.src = 'images/point.png';
-point_image.onload = function(){
-ctx.drawImage(point_image, 230, 300, 170, 150);
-}
-
-function press(event) {
-    if(event== 37) {//LEFT
-      x = x - 1;
-    } else if(event == 38) {//UP
-      y = y - 1;
-    } else if(event ==39) {//RIGHT
-      x = x + 1;
-    } else if(event== 40) {//DOWN
-      y = y + 1;
+class game{
+  constructor(){
+    this.canvas = document.getElementById("myCanvas");
+    this.ctx = this.canvas.getContext("2d");
+    this.rock_image = new Image();
+    this.rock_image.src = 'images/rock.png';
+    this.paper_image = new Image();
+    this.paper_image.src = 'images/paper.png';
+    this.scissor_image = new Image();
+    this.scissor_image.src = 'images/scissor.png';
+    this.point_image = new Image();
+    this.point_image.src = 'images/point.png';
+    onload=()=>{
+      this.ctx.drawImage(this.rock_image, 20, 20, 170, 130);
+      this.ctx.drawImage(this.paper_image, 210, 20, 170, 130);
+      this.ctx.drawImage(this.scissor_image, 400, 20, 170, 130);
+      this.ctx.drawImage(this.point_image, 230, 300, 170, 150);
     }
-    draw();
   }
-  function draw(){
-    ctx.drawImage(point_image, 230, 300, 170, 150);
-    }
-    addEventListener("keydown", function(){
-        press(event.keyCode);
+}
+let object = new game();
+
+
+// function press(event) {
+//     if(event== 37) {//LEFT
+//       x = x - 1;
+//     } else if(event == 38) {//UP
+//       y = y - 1;
+//     } else if(event ==39) {//RIGHT
+//       x = x + 1;
+//     } else if(event== 40) {//DOWN
+//       y = y + 1;
+//     }
+//     draw();
+//   }
+//   function draw(){
+//     ctx.drawImage(point_image, 230, 300, 170, 150);
+//     }
+//     addEventListener("keydown", function(){
+//         press(event.keyCode);
         
-    });
+//     });
