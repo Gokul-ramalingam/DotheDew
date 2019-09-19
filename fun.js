b46b1a44299e4e2f69ff7c32f8942e59109c913c
Pratikshatippe <54614091+Pratikshatippe@users.noreply.github.com>
2019-09-19 13:35:48 +0530

    create folder and move

--- fun.js
+++ fun.js
@@ -1,22 +1,47 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+// rock is displayed
 rock_image = new Image();
-rock_image.src = 'rock.png';
+rock_image.src = 'images/rock.png';
 rock_image.onload = function(){
 ctx.drawImage(rock_image, 20, 20, 170, 130);
 }
+// this image for paper
 paper_image = new Image();
-paper_image.src = 'paper.png';
+paper_image.src = 'images/paper.png';
 paper_image.onload = function(){
 ctx.drawImage(paper_image, 210, 20, 170, 130);
 }
+// this image for scissor
 scissor_image = new Image();
-scissor_image.src = 'scissor.png';
+scissor_image.src = '/images/scissor.png';
 scissor_image.onload = function(){
 ctx.drawImage(scissor_image, 400, 20, 170, 130);
 }
+// this for point to above images
 point_image = new Image();
-point_image.src = 'point.png';
+
+point_image.src = 'images/point.png';
 point_image.onload = function(){
 ctx.drawImage(point_image, 230, 300, 170, 150);
 }
+
+function press(event) {
+    if(event== 37) {//LEFT
+      x = x - 1;
+    } else if(event == 38) {//UP
+      y = y - 1;
+    } else if(event ==39) {//RIGHT
+      x = x + 1;
+    } else if(event== 40) {//DOWN
+      y = y + 1;
+    }
+    draw();
+  }
+  function draw(){
+    ctx.drawImage(point_image, 230, 300, 170, 150);
+    }
+    addEventListener("keydown", function(){
+        press(event.keyCode);
+        
+    });
