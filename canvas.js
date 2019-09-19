cad2aea40dd899cc2b8b93f5fb6800a478e89e5c
Gokul <gokulramalingam1@gmail.com>
2019-09-19 07:26:52 +0530

    Implementation of score calculation

--- canvas.js
+++ canvas.js
@@ -12,7 +12,8 @@ class Options{
     this.paperImage.src = 'images/paper.png';
     this.scissorImage.src = 'images/scissor.png';
     this.selectImage.src = 'images/select.png';
-    this.ar = [];
+    this.optionSelected = 0;
+    this.random = 0;
     this.x = 425;
     this.y = 390;
     onload=()=>{
@@ -21,14 +22,38 @@ class Options{
         this.ctx.drawImage(this.scissorImage,670,25,100,100);
         this.ctx.drawImage(this.selectImage,this.x,this.y,100,100);
     } 
-    this.ar.push(this.selectImage);
     this.keyPress = function(e){
      e==37?this.x-=5:e==38?this.y-=5:e==39?this.x+=5:this.y+=5;
        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
        this.ctx.drawImage(this.rockImage,175,25,100,100);
        this.ctx.drawImage(this.paperImage,425,25,100,100);
        this.ctx.drawImage(this.scissorImage,670,25,100,100);
-       console.log(this.ctx.drawImage(this.selectImage,this.x,this.y,100,100));
+       this.ctx.drawImage(this.selectImage,this.x,this.y,100,100);
+       this.random= Math.floor(Math.random()*3)+1;
+       if(this.x == 175)
+       {  this.x = 425;
+          this.y = 390;
+          this.optionSelected = 1;
+        }
+       else if(this.x == 425)
+        {   this.x = 425;
+            this.y = 390;
+            this.optionSelected = 2;
+        }
+       else if(this.x == 670){
+             this.x = 425;
+             this.y = 390;
+             this.optionSelected = 3;
+            }
+      if(this.random==this.optionSelected){
+         let points = document.getElementById('player').innerText;
+         document.getElementById('player').innerHTML=1+parseInt(points);
+         this.optionSelected=0;
+      }
+      else{
+         let point = document.getElementById('computer').innerText;
+          document.getElementById('computer').innerHTML=1+parseInt(point);
+      }
     }
 }
 
