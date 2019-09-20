e0a9a005d85fcc8760fc8d30853239447aa13129
Pratikshatippe <54614091+Pratikshatippe@users.noreply.github.com>
2019-09-20 12:27:13 +0530

    added background image

--- fun.js
+++ fun.js
@@ -2,8 +2,8 @@ class game{
   constructor(){
     this.canvas = document.getElementById("myCanvas");
     this.ctx = this.canvas.getContext("2d");
-    this.bg_image = new  image();
-    this.bg_image.src = 'images/background'
+    this.bg_image = new  Image();
+    this.bg_image.src = 'images/background.jpg'
     this.rock_image = new Image();
     this.rock_image.src = 'images/rock.png';
     this.paper_image = new Image();
@@ -11,7 +11,7 @@ class game{
     this.scissor_image = new Image();
     this.scissor_image.src = 'images/scissor.png';
     this.point_image = new Image();
-    this.point_image.src = 'images/point.png';
+    this.point_image.src = 'images/player.png';
     this.x = 230;
     this.y = 300;
     onload=()=>{
@@ -32,6 +32,7 @@ class game{
             this.y = this.y + 5;
           }
           this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+          this.ctx.drawImage(this.bg_image, 0, 0, 600, 500);
           this.ctx.drawImage(this.rock_image, 20, 20, 170, 130);
           this.ctx.drawImage(this.paper_image, 210, 20, 170, 130);
           this.ctx.drawImage(this.scissor_image, 400, 20, 170, 130);
