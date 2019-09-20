f66b6cfcd0684a2f41f526cc973f668a2d5a2edd
Gokul <gokulramalingam1@gmail.com>
2019-09-20 17:24:17 +0530

    Implementation of changes

--- canvas.js
+++ canvas.js
@@ -4,6 +4,8 @@ class Options{
     this.canvas.width = 570;
     this.canvas.height = 400;
     this.ctx = this.canvas.getContext('2d');
+    this.ctx.fillStyle="#7DEEFE"
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
     this.rockImage =  new Image();
     this.paperImage  = new Image();
     this.scissorImage = new Image();
@@ -11,39 +13,48 @@ class Options{
     this.rockImage.src = 'images/rock.png';
     this.paperImage.src = 'images/paper.png';
     this.scissorImage.src = 'images/scissor.png';
-    this.selectImage.src = 'images/select.png';
+    this.selectImage.src = 'images/pikachu.png';
     this.random = 0;
     this.x = 220;
     this.y = 300;
+    this.pokePick = new Audio("pickup.mp3");
+    this.pokePick.volume = 0.8;
     onload=()=>{
         this.locationSpecifier();
     } 
+
     this.keyPress = function(e){
      e==37?this.x-=5:e==38?this.y-=5:e==39?this.x+=5:this.y+=5;
        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+       this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
        this.locationSpecifier();
        
        if((this.x >= 0 && this.x<=70) && (this.y >=20 && this.y<=50 ))
-       {  this.reset();
+       {  
+          this.pokePick.play();
+          this.reset();
           this.validator(1,2,3);
         }
        else if((this.x >= 200&& this.x <= 250) && (this.y >=20 && this.y<=50 ))
         {   
+            this.pokePick.play();
             this.reset();
             this.validator(2,1,3);
         }
        else if((this.x >= 430 && this.x<=470) && (this.y >=20 && this.y<=50)){
+            this.pokePick.play();
             this.reset();
             this.validator(3,1,2);
             }
     }
-    this.reset = function(){
+       this.reset = function(){
        this.x = 220;
        this.y = 300;
+    //    this.pokePick.pause();
      }
 
     this.validator = function(optionSelected,a,b){
-        this.random= generateRandomNumber(optionSelected);
+        this.random = generateRandomNumber(optionSelected);
             switch(this.random){
                 case a:document.getElementById('computer').innerHTML=1+parseInt(document.getElementById('computer').innerText);
                        break;
@@ -84,5 +95,3 @@ addEventListener("keydown",function(event){
     let rand = Math.floor(Math.random()*3)+1;
     return rand === optionSelected? generateRandomNumber(optionSelected) : rand;
 }
-
-/*here*/
