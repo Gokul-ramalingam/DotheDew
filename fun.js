e24cc6118276df5302f0eb5b699dee1bb3b6544a
Pratikshatippe <54614091+Pratikshatippe@users.noreply.github.com>
2019-09-20 10:22:34 +0530

    object moved

--- fun.js
+++ fun.js
@@ -10,33 +10,32 @@ class game{
     this.scissor_image.src = 'images/scissor.png';
     this.point_image = new Image();
     this.point_image.src = 'images/point.png';
+    this.x = 230;
+    this.y = 300;
     onload=()=>{
       this.ctx.drawImage(this.rock_image, 20, 20, 170, 130);
       this.ctx.drawImage(this.paper_image, 210, 20, 170, 130);
       this.ctx.drawImage(this.scissor_image, 400, 20, 170, 130);
-      this.ctx.drawImage(this.point_image, 230, 300, 170, 150);
+      this.ctx.drawImage(this.point_image, this.x, this.y, 170, 150);
     }
+    this.move = function(event) {
+          if(event== 37) {//LEFT
+            this.x = this.x - 1;
+          } else if(event == 38) {//UP
+            this.y = this.y - 1;
+          } else if(event ==39) {//RIGHT
+            this.x = this.x + 1;
+          } else if(event== 40) {//DOWN
+            this.y = this.y + 1;
+          }
+          this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+          this.ctx.drawImage(this.point_image, this.x, this.y, 170, 150);
   }
 }
+}
+
 let object = new game();
-
-
-// function press(event) {
-//     if(event== 37) {//LEFT
-//       x = x - 1;
-//     } else if(event == 38) {//UP
-//       y = y - 1;
-//     } else if(event ==39) {//RIGHT
-//       x = x + 1;
-//     } else if(event== 40) {//DOWN
-//       y = y + 1;
-//     }
-//     draw();
-//   }
-//   function draw(){
-//     ctx.drawImage(point_image, 230, 300, 170, 150);
-//     }
-//     addEventListener("keydown", function(){
-//         press(event.keyCode);
-        
-//     });
+addEventListener("keydown", function(e){
+  object.move(e.keyCode);
+  // console.log("here");
+});
